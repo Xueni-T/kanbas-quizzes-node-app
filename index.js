c5adff7c0640b9b25d17bda9ea7313c08ca0084e
tangxueni <tang.xuen@northeastern.edu>
2024-11-23 03:57:46 -0500

    first commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(
     origin: process.env.NETLIFY_URL || "http://localhost:3000",
   })
  );
-
+ app.use(express.json());
  const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kanbas",
   resave: false,
