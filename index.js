e3b4e74bcebc61c807eb94990910edce7863aef5
tangxueni <tang.xuen@northeastern.edu>
2024-12-01 13:28:51 -0500

    A6 first commit

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import session from "express-session";
+import mongoose from "mongoose";
 import "dotenv/config";
 import UserRoutes from "./Kanbas/Users/routes.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import EnrollmentsRoutes from "./Kanbas/Enrollments/routes.js";
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 
@@ -17,22 +21,25 @@ app.use(
     credentials: true,
     origin: process.env.NETLIFY_URL || "http://localhost:3000",
   })
- );
- app.use(express.json());
- const sessionOptions = {
+);
+
+app.use(express.json());
+
+import MongoStore from 'connect-mongo';
+
+const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kanbas",
   resave: false,
   saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URI || 'mongodb://localhost:27017/kanbas',
+    collectionName: 'sessions'
+  }),
+  cookie: {
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 1000 * 60 * 60 * 24 // 1 day
+  }
 };
-
-if (process.env.NODE_ENV !== "development") {
-  sessionOptions.proxy = true;
-  sessionOptions.cookie = {
-    sameSite: "none",
-    secure: true,
-    domain: process.env.NODE_SERVER_DOMAIN,
-  };
-}
 app.use(session(sessionOptions));
 
 UserRoutes(app);
